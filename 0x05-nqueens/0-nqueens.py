eaa3c362cf6fc48ff70eaf53334a8f3888f35195
MalekAjil <aboanasajil@gmail.com>
2024-11-10 12:26:28 +0000

    task0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,8 +3,13 @@
 import sys
 
 
-if sys.arg[1] is None:
+if len(sys.args) != 2 or sys.args[1] is None:
+    print("Usage: nqueens N")
+    exit(1)
+N = sys.args[1]
+if type(N) is not int:
     print("N must be a number")
     exit(1)
-nqueen = sys.arg[1]
-
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
