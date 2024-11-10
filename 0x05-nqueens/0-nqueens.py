8b66c15284454b1db9fc84ec356364475724928d
MalekAjil <aboanasajil@gmail.com>
2024-11-10 16:02:39 +0000

    task0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,14 +3,51 @@
 import sys
 
 
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+def solve_nqueens(board, col):
+    """Use backtracking to find all solutions"""
+    if col >= len(board):
+        print_solution(board)
+        return True
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res board[i][col] = 0
+    return res
+
+def print_solution(board):
+    """Print the board configuration"""
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
 if len(sys.argv) != 2 or sys.argv[1] is None:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 try:
     N = int(sys.argv[1])
 except:
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 if N < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
+board = [[0 for _ in range (N)] for - in range(N)]
+solve_nqueens(board, 0)
