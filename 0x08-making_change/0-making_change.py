d2220607593c23daf995e2f8c7c97c0a21e9c715
MalekAjil <aboanasajil@gmail.com>
2024-11-27 04:46:12 +0000

    task0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """Making Change Module"""
 
+
+def makeChange(coins, total):
+    """Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1"""
+    if total <= 0:
+        return 0
