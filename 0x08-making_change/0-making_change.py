110aa672a54699ce2416e824175edf2895cf7bb3
MalekAjil <aboanasajil@gmail.com>
2024-11-28 20:08:32 +0000

    task0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,3 +8,11 @@ def makeChange(coins, total):
     If total cannot be met by any number of coins you have, return -1"""
     if total <= 0:
         return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
